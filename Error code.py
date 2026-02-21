ae62a3077dc17ef21bf0264b025cacfa4ece5259
Yash Parmar <yashsparmar254@gmail.com>
2026-02-21 15:33:07 +0530

    Create Error code.py

---
+++ Error code.py
@@ -0,0 +1,22 @@
+print("Fibonacci Series Analyzer")  
+n = int(input("Enter limit: ")) 
+a = 0 
+b = 1 
+count = 0 
+sum_total = 0 
+while count < n:  
+print(a) 
+sum_total = sum_total + a 
+next = a + b 
+a = b 
+b = next 
+count = count + 1 
+if sum_total % 2 = 0:  
+print("Even sum") 
+else: 
+print("Odd sum")  
+print("Total Sum:", sum_total 
+if sum_total > 50:  
+print("Large series") 
+else: 
+print("Small series")    write down all line eror
